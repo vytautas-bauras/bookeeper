28d4f3686d28d9c16e8f177029e49ef4c3c5679a
Vytautas Bauras <vytautas.bauras.612@gmail.com>
2020-06-21 22:21:24 +0300

    add more test logic

--- src/Implementation/DoubleEntry/AccountsBalance/AccountsBalanceCalculator/TemporaryAccountsBalanceMapInitializer.test.ts
+++ src/Implementation/DoubleEntry/AccountsBalance/AccountsBalanceCalculator/TemporaryAccountsBalanceMapInitializer.test.ts
@@ -1,22 +1,71 @@
 import tap from 'tap';
 
-const accountsReceivableAccount = {
-    getAccountCode() {
-        ""
+const childAccounts = [
+    {
+        getAccountCode() {
+            return "101";
+        },
+        getAccountTitle() {
+            return "Cash"
+        },
+        getChildAccounts() {
+            return [];
+        },
+        getParentAccount() {
+            return accountList["100"];
+        }
+    },
+    {
+        getAccountCode() {
+            return "120";
+        },
+        getAccountTitle() {
+            return "Accounts Receivable"
+        },
+        getChildAccounts() {
+            return [];
+        },
+        getParentAccount() {
+            return accountList["100"];
+        }
     }
+];
+
+const assetsAccount = {
+    getAccountCode() {
+        return "100"
+    },
+    getAccountTitle() {
+        return "Assets";
+    },
+    getChildAccounts() {
+        return childAccounts;
+    },
+    getParentAccount() {
+        return undefined;
+    }
+};
+
+const accountList: ChartAccountList = {
+    "100": assetsAccount,
+    "101": childAccounts[0],
+    "120": childAccounts[1]
+};
+
+function validateTemporaryAccountBalance(balance: TemporaryLedgerAccountBalance<any>, expectedAccount: ChartAccount) {
+    tap.equals(balance.account, assetsAccount);
+    tap.equals(balance.balance, 0);
+    tap.equals(balance.credit, 0);
+    tap.equals(balance.debit, 0);
 }
 
 const initializer = new TemporaryAccountsBalanceMapInitializer({
     getAccountList() {
-        return {
-            "100 - Assets": {
-                getAccountCode() {
-                    return "Assets"
-                },
-                getChildAccounts() {
-
-                }
-            }
-        }
+        return accountList
     }
 } as ChartAccountListProvider);
+
+const map = initializer.initializeTemporaryAccountsBalanceMap();
+validateTemporaryAccountBalance(map["100"], assetsAccount);
+validateTemporaryAccountBalance(map["101"], childAccounts[0]);
+validateTemporaryAccountBalance(map["120"], childAccounts[1]);
