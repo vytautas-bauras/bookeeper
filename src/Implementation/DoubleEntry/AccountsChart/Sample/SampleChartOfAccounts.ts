4912173386bf8de797234e44bf274a466163101e
Vytautas Bauras <vytautas.bauras.612@gmail.com>
2020-06-22 00:58:12 +0300

    add a sample CoA

---
+++ src/Implementation/DoubleEntry/AccountsChart/Sample/SampleChartOfAccounts.ts
@@ -0,0 +1,33 @@
+import InMemoryAccountsChart from "../InMemory/InMemoryAccountsChart";
+import InMemoryAssetsAccount from "../InMemory/InMemoryAssetsAccount";
+import InMemoryLiabilitiesAccount from "../InMemory/InMemoryLiabilitiesAccount";
+import InMemoryEquityAccount from "../InMemory/InMemoryEquityAccount";
+import InMemoryRevenuesAccount from "../InMemory/InMemoryRevenuesAccount";
+import InMemoryExpensesAccount from "../InMemory/InMemoryExpensesAccount";
+
+export default new InMemoryAccountsChart(
+    new InMemoryAssetsAccount(
+        "100",
+        "Assets",
+        [
+            new InMemoryAssetsAccount("101","Cash"), 
+            new InMemoryAssetsAccount("120","Accounts Receivable")
+        ]
+    ),
+    new InMemoryLiabilitiesAccount(
+        "200",
+        "Liabilities"
+    ),
+    new InMemoryEquityAccount(
+        "300",
+        "Equity"
+    ),
+    new InMemoryRevenuesAccount(
+        "400",
+        "Revenues"
+    ),
+    new InMemoryExpensesAccount(
+        "500",
+        "Expenses"
+    )
+);
