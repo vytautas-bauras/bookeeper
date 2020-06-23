fa2c1fe8dd778cc3687a95cb6b3075560f4db0d9
Vytautas Bauras <vytautas.bauras.612@gmail.com>
2020-06-23 09:49:17 +0300

    categorize accounts by debit/credit

---
+++ src/Contract/CoreConcepts/DoubleEntry/AccountsChart/DebitCreditChartAccounts.ts
@@ -0,0 +1,4 @@
+import { ChartAccountType } from "./ChartAccountType";
+
+export const DEBIT_ACCOUNTS = [ChartAccountType.Assets, ChartAccountType.Expenses];
+export const CREDIT_ACCOUNTS = [ChartAccountType.Equity, ChartAccountType.Liabilities, ChartAccountType.Revenues];
