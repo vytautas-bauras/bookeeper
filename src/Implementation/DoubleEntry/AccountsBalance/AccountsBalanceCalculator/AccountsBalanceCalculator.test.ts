2915f7cded489f63101dd03ed3180249f42080e8
Vytautas Bauras <vytautas.bauras.612@gmail.com>
2020-06-25 11:59:55 +0300

    test accounts balance calculator

---
+++ src/Implementation/DoubleEntry/AccountsBalance/AccountsBalanceCalculator/AccountsBalanceCalculator.test.ts
@@ -0,0 +1,13 @@
+import tap from "tap";
+import SearchableAccountsChartAdapter from "../../AccountsChart/SearchableAccountsChart/SearchableAccountsChartAdapter";
+import SampleChartOfAccounts from "../../AccountsChart/Sample/SampleChartOfAccounts";
+import AccountsBalanceCalculator from "./AccountsBalanceCalculator";
+import SampleTransactions from "../../Ledger/Sample/SampleTransactions";
+import { ChartAccountType } from "../../../../Contract/CoreConcepts/DoubleEntry/AccountsChart/ChartAccountType";
+
+const searchableAccountsChart = new SearchableAccountsChartAdapter(SampleChartOfAccounts);
+const calculator = new AccountsBalanceCalculator(searchableAccountsChart);
+
+const balance = calculator.calculateAccountsBalance(SampleTransactions());
+const assetsBalance = balance.getRootBalanceByAccountType(ChartAccountType.Assets);
+tap.equals(assetsBalance.getBalance(), 500);
