ac837436eb386a3635a02956cda34d4120f22c4a
Vytautas Bauras <vytautas.bauras.612@gmail.com>
2020-06-27 17:28:53 +0300

    override property to match class

--- src/Implementation/DoubleEntry/AccountsChart/AccountMapping/SerializableMappedChartAccount.ts
+++ src/Implementation/DoubleEntry/AccountsChart/AccountMapping/SerializableMappedChartAccount.ts
@@ -2,4 +2,5 @@ import SerializableAccount from "../Serializable/SerializableAccount";
 
 export default interface SerializableMappedChartAccount extends SerializableAccount {
     mappedAccountCodes: string[];
+    childAccounts: SerializableMappedChartAccount[]
 }
